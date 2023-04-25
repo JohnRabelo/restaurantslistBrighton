8c388d8ec1fab6a69e60f88eec5355406070b501
johnr21 <johnrabelo.contact@gmail.com>
2023-04-25 19:11:16 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route} from "react-router-dom"
+import HomePg from "./pages/Home";
+import ContactPg from "./pages/Contact";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePg/>} />
+        <Route path='contact' element={<ContactPg/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Contact.js
@@ -0,0 +1,12 @@
+import contactcl from './Contact.module.css'
+
+function ContactPg() {
+    return (  
+      <div>
+    <div>Get in touch with us!</div>
+    <div> Form</div>
+      </div>
+    );
+  }
+  
+  export default ContactPg;
