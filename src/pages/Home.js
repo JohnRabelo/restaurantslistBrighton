e7d81e3d158ef0c242e8894f21abebf2d7eaca01
johnr21 <johnrabelo.contact@gmail.com>
2023-04-26 17:12:48 -0300

    updates2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import homecl from './Home.module.css'
 function HomePg() {
     return (  
       <div>
-      <div>Select one of the restaurants from the list to get more info! </div>
+      <div>Select one of the restaurants from the list to get more info! Updates soon </div>
       <div>Project by <span className={homecl.creators}>John Rabelo</span></div>
       </div>
     );
