d88276e388b56668ac435d8f7d08ecbb6093c02f
johnr21 <johnrabelo.contact@gmail.com>
2023-04-28 19:32:58 -0300

    hamburguerNavMenu

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,51 @@
 import homecl from './Home.module.css'
+import banner1 from '../assets/banner1.jpg'
+import banner2 from '../assets/banners2.jpg'
+import { useState } from 'react';
+
 
 function HomePg() {
+
+  const [slideMove, setSlideMove] = useState(false);
+
+  function switchSlide(){
+    const windowSize = window.innerWidth;
+    setSlideMove(current => !current);
+    if (windowSize > 450){
+      if (slideMove){
+        return document.querySelector('span').style.marginLeft = '0vw';
+      }
+      else{
+        return document.querySelector('span').style.marginLeft = '-80vw';
+      }
+    }
+  }
+  
+
+
     return (  
-      <div className={homecl.homeContainer}>
-        <p>Select one of the restaurants from the list to get more info! Updates coming soon</p>
+      <div>
+        <div className={homecl.BannerOrganizer}>
+          <div className={homecl.BannerContainer}>
+            <span className={homecl.BannerDisplay}>
+              <div>
+                <img src={banner1} alt=''/>
+                <img src={banner2} alt=''/>
+              </div>
+            </span>
+          </div>
+        </div>
+        <button onClick={switchSlide}>Next</button>
+        <div>gfegn<br/>ltr<br/><br/><br/><br/><br/><br/><br/>nilnil vrevrmgigetileigilremlwmgwllmtelgmegmermg
+          regmoemgeçgmegmeçwgbm
+
+          emroegrmog
+          gmogmegro
+
+
+
+          gre,pqo,
+        </div>
       </div>
     );
   }
--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -3,21 +3,39 @@ import BRBLogo from '../assets/BRBLogo.png'
 import MexLogo from '../assets/MexLogo.png' 
 import hbgIcon from '../assets/hbg.JPG'
 import {Outlet, NavLink} from "react-router-dom"
+import { useState } from 'react'
+
 
 function NavBar() {
+
+    const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+    function switchMenu(){
+      const windowSize = window.innerWidth;
+      setMenuIsOpen(current => !current);
+      if (windowSize < 450){
+        if (menuIsOpen){
+          return document.querySelector('nav').style.display = 'flex';
+        }
+        else{
+          return document.querySelector('nav').style.display = 'none';
+        }
+      }
+    }
+
     return (  
       <div>
         <div className={navcl.navOrganizer}>
           <div className={navcl.navLogo}>
             <img src={BRBLogo} alt='' />
           </div>
-          <div className={navcl.navMenu}>
+          <nav className={navcl.navMenu}>
             <ul>
-              <li><NavLink to='/' className={navcl.navigLink}> Home</NavLink></li>
-              <li><NavLink to='/contact' className={navcl.navigLink}>Contact</NavLink></li>
+              <li><NavLink to='/' className={navcl.navigLink} onClick={switchMenu}> Home</NavLink></li>
+              <li><NavLink to='/contact' className={navcl.navigLink} onClick={switchMenu}>Contact</NavLink></li>
             </ul>
-          </div>
-          <div className={navcl.menuOpenerContainer}>
+          </nav>
+          <div className={navcl.menuOpenerContainer} onClick={switchMenu}>
             <img src={hbgIcon} alt='' />
           </div>
           <div className={navcl.navLogo2}>
