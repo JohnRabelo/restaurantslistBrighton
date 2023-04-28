2cfa47bcf4313bd038fe8d1b6deb8ba3e646853c
johnr21 <johnrabelo.contact@gmail.com>
2023-04-28 20:11:15 -0300

    sliderWorking

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import homecl from './Home.module.css'
 import banner1 from '../assets/banner1.jpg'
 import banner2 from '../assets/banners2.jpg'
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 
 
 function HomePg() {
 
-  const [slideMove, setSlideMove] = useState(false);
+  /*const [slideMove, setSlideMove] = useState(false);
 
   function switchSlide(){
     const windowSize = window.innerWidth;
@@ -19,35 +19,48 @@ function HomePg() {
         return document.querySelector('span').style.marginLeft = '-80vw';
       }
     }
-  }
+  }*/
+
+  const [current, setCurrent] = useState(0);
   
 
+  useEffect(()=>{
+    let sldLength = 2;
+    const windowSize = window.innerWidth;
+    if (windowSize > 450){
+      const next = (current + 1) % sldLength;
+      let newMargin = next * 80;
+      const id = setTimeout(() => setCurrent(next), 3600);
+      document.querySelector('span').style.marginLeft = -newMargin+'vw' ;
+      return () => clearTimeout(id);
+    } 
+  }, [current]);
 
-    return (  
-      <div>
-        <div className={homecl.BannerOrganizer}>
-          <div className={homecl.BannerContainer}>
-            <span className={homecl.BannerDisplay}>
-              <div>
-                <img src={banner1} alt=''/>
-                <img src={banner2} alt=''/>
-              </div>
-            </span>
-          </div>
-        </div>
-        <button onClick={switchSlide}>Next</button>
-        <div>gfegn<br/>ltr<br/><br/><br/><br/><br/><br/><br/>nilnil vrevrmgigetileigilremlwmgwllmtelgmegmermg
-          regmoemgeçgmegmeçwgbm
-
-          emroegrmog
-          gmogmegro
-
-
-
-          gre,pqo,
+  return (  
+    <div>
+      <div className={homecl.BannerOrganizer}>
+        <div className={homecl.BannerContainer}>
+          <span className={homecl.BannerDisplay}>
+            <div>
+              <img src={banner1} alt=''/>
+              <img src={banner2} alt=''/>
+            </div>
+          </span>
         </div>
       </div>
-    );
+      <button>Next</button>
+      <div>gfegn<br/>ltr<br/><br/><br/><br/><br/><br/><br/>nilnil vrevrmgigetileigilremlwmgwllmtelgmegmermg
+        regmoemgeçgmegmeçwgbm
+
+        emroegrmog
+        gmogmegro
+
+
+
+        gre,pqo,
+      </div>
+    </div>
+  );
   }
   
   export default HomePg;
